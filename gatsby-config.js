b967a6632835c076e570748e59cdb6569efcb081
Collins Zimba <collinszim@gmail.com>
2019-05-19 10:16:28 +0200

    Updated the Blog Title & Author

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
+    title: `That blog MadeByCollins`,
+    author: `Collins Zimba`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
